314fbe055221213c66c00d775856c98c48e30dad
J.C. Moyer <jcmoyer32@gmail.com>
2025-09-19 18:17:47 -0400

    cleanup

--- src/backend/decoder2/dispatch_aa.cpp
+++ src/backend/decoder2/dispatch_aa.cpp
@@ -2,12 +2,10 @@
 
 #include "dispatch.h"
 #include "dispatch_address.h"
-#include "dispatch_handlers.h"
 
 constexpr std::array<std::array<D_OpcodeHandler, 256>, 2> DECODE_TABLES_AA8{{
     DefineGenericTable<MCU_Operand_Size::BYTE, Mode_Aa8>(),
     DefineGenericTable<MCU_Operand_Size::WORD, Mode_Aa8>(),
-
 }};
 
 D_OpcodeHandler GetDispatcherAa8(uint8_t opcode, MCU_Operand_Size size)
