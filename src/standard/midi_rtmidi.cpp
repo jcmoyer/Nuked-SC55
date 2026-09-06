062ea4926338aa55972e35c366aa6ce2dea26d9d
J.C. Moyer <jcmoyer32@gmail.com>
2026-09-06 14:34:37 -0400

    Relicense as GPL-2.0-or-later
    This is a roll-up of two upstream commits which change the license:
    
    https://github.com/nukeykt/Nuked-SC55/commit/f6e50220d5de4241080e1811a30d29f7113a0929
    https://github.com/nukeykt/Nuked-SC55/commit/bc7d954ef3e04974d68ccf5fc934472eeef8b57d
    
    Additionally, I license my modifications under the same license.
    
    All source files now have a license header, and copyright assignment
    is done on a best effort basis. Modifications to source files which
    are still significantly derived from upstream sources are indicated in
    the license header. Please open an issue if you think a file is
    misattributed.

--- src/standard/midi_rtmidi.cpp
+++ src/standard/midi_rtmidi.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2021, 2024 nukeykt
+ * Modified by J.C. Moyer
+ * Original source file: src/midi_rtmidi.cpp
+ *
+ * This file is part of Nuked-SC55.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 #include "midi.h"
 #include "common/command_line.h"
 #include <RtMidi.h>
